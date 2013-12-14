8be8b1f07358852ee56c1b7acde48786b352dbf1
wolf1994722 <herlmercedes@gmail.com>
2013-12-13 21:23:34 -0500

    pretty html

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
+  app.locals.pretty = true;
 }
 
 app.get('/', routes.index);
